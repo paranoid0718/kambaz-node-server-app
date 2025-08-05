04ebd7b387605cacb7e30c6f455e23ae8f65113b
赵同霄 <thewish0718@gmail.com>
2025-08-05 19:52:42 -0400

    fix: update CORS for Netlify frontend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.NETLIFY_URL || "http://localhost:5173",
 })); 
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
